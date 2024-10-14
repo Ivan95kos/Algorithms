d2c7c715c91b7995fce882dc1c3b05da808633c9
ivan <ivan95kos@gmail.com>
2024-10-15 00:10:11 +0200

    Minimum Stack

---
+++ src/main/java/_6_Minimum_Stack/MinimumStack.java
@@ -0,0 +1,46 @@
+package _6_Minimum_Stack;
+
+
+import java.util.Arrays;
+import java.util.Objects;
+
+// https://neetcode.io/problems/minimum-stack
+
+public class MinimumStack {
+
+    private final static int DEFAULT_SIZE = 10;
+    private Integer[] stack;
+    private int index = 0;
+    private int minValue = Integer.MAX_VALUE;
+
+    public MinimumStack() {
+        stack = new Integer[DEFAULT_SIZE];
+    }
+
+    public void push(int val) {
+        if (stack.length <= index) {
+            stack = Arrays.copyOf(stack, stack.length * 2);
+        }
+
+        if (val < minValue) {
+            minValue = val;
+        }
+
+        stack[index++] = val;
+    }
+
+    public void pop() {
+        stack[index-- - 1] = null;
+    }
+
+    public int top() {
+        return stack[index - 1];
+    }
+
+    public int getMin() {
+        return Arrays.stream(stack)
+                .filter(Objects::nonNull)
+                .min(Integer::compareTo)
+                .get();
+    }
+}
